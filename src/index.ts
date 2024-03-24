957de60f6044a977b3da7e8bca8b239b7ca0934c
Max Netterberg <max.netterberg@gmail.com>
2024-03-24 16:17:17 +0100

    export correlation id provider interface

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { LogLevel } from "./LogLevel.js";
 export type { JsonSerializable, JsonArray, LogEntry } from "./LogEntry.js";
 export { ConsoleLogFormatter } from "./formatters/ConsoleLogFormatter.js";
 export { GoogleCloudRunLogFormatter } from "./formatters/GoogleCloudRunLogFormatter.js";
+export type { CorrelationIdProvider } from "./Logger.js";
